bcedca4a6e676c05f890039ea1789ebfefcdd3a9
Noah Timothy Keba <144520555+m5botofnoahtimothy14@users.noreply.github.com>
2026-07-09 06:31:39 +0000

    plyer fixes

--- core/ui/notification_handler.py
+++ core/ui/notification_handler.py
@@ -1,7 +1,11 @@
 ﻿                            
 import threading
 import time
-from plyer import notification
+try:
+    from plyer import notification
+    PLYER_AVAILABLE = True
+except ImportError:
+    PLYER_AVAILABLE = False
 
 class NotificationHandler:
     def __init__(self):
@@ -11,6 +15,8 @@ class NotificationHandler:
         threading.Thread(target=self._notify_thread, args=(title, message, duration), daemon=True).start()
 
     def _notify_thread(self, title, message, duration):
+        if not PLYER_AVAILABLE:
+            return
         notification.notify(
             title=title,
             message=message,
